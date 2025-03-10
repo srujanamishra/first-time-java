45490b8b240c3deccdd8486754d07283d856de02
Srujana Mishra <127395073+srujanamishra@users.noreply.github.com>
2025-03-10 22:23:01 +0530

    Add files via upload

---
+++ lab2q1.java
@@ -0,0 +1,8 @@
+// WAP in java which will accept KIIT UNIVERSITY as the command argument and display the same.//
+
+public class lab2q1{
+    public static void main(String[] args) {
+        System.out.println(args[5]);
+        System.out.println(args[3]);
+    }
+}
